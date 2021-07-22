762dcb21985b729b6e0fc8bd72e521cd17dc8fca
tomasbabikian <tomibabikian@gmail.com>
2021-07-22 15:56:14 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+alert ("¡Hola! a continuación te solicitaremos algunos datos")
+let nombre = prompt ("ingresa tu nombre");
+let edad = prompt ("ingresa tu edad") ;
+
+
+console.log (nombre + " tiene " + edad)
+let resultado = alert (nombre + " tiene " + edad)
